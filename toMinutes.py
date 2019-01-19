cf5762a0963be979435bdfc476b4bd495024c9ce
星野真広 <foxeyes1510@gmail.com>
2019-01-19 21:47:27 +0900

    create toMinutes.py

---
+++ toMinutes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.4
+# -*- coding: utf-8 -*-
+ 
+import cgi
+import sys
+import io
+ 
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+ 
+form = cgi.FieldStorage()
+  
+text_data = form.getvalue('text', 'no data')
+  
+print('Content-type: text/html; charset=UTF-8')
+print('')
+print(text_data)
