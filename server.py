da7b6b35d166d90735d784b721ba9f587d7ec072
星野真広 <foxeyes1510@gmail.com>
2019-01-24 13:24:26 +0900

    リセット導入

--- server.py
+++ server.py
@@ -1,38 +1,50 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, Response, jsonify
+import datetime
+import json
 import os
 
 app = Flask(__name__)
-
+text_list = []
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
 
-@app.route("/getminutes")
-def get_minutes():
-    dirList = os.listdir("./minutes")
-    return render_template('getminutes.html', list=dirList)
+@app.route("/reset", methods=["POST"])
+def reset():
+    text_list.clear()
+    return ""
 
 
-@app.route('/download/<file_name>', methods=['GET'])
-def download(file_name):
-    response = make_response()
-    response.data = open("./minutes/" + file_name, "rb").read()
-    downloadFileName = file_name
-    response.headers['Content-Disposition'] = 'attachment; filename=' + downloadFileName
-    return response
-
-
-@app.route("/post", methods=["GET", "POST"])
+@app.route("/post", methods=["POST"])
 def post():
     if request.method == "POST":
+        global text_list
         name = request.form['name']
         text = request.form['text']
-        time = request.form['time']
+
+        year = int(request.form['year'])
+        month = int(request.form['month'])
+        day = int(request.form['day'])
+        hour = int(request.form['hour'])
+        minute = int(request.form['minute'])
+        second = int(request.form['second'])
+        millisecond = int(request.form['millisecond'])
+
+        time = datetime.datetime(year, month, day, hour, minute, second, millisecond * 1000).strftime("%Y-%m-%d %H:%M:%S.%f")
+
         file = open('minutes/test.txt', 'a', encoding='utf-8')
         file.write(name + "," + text + "," + time + "\n")
-        return ""
+
+        text_list.append({
+                'name' : name,
+                'text' : text,
+                'time' : time
+        })
+        text_list = sorted(text_list, key=lambda x: x['time'])
+
+        return make_response(json.dumps(text_list, ensure_ascii=False))
 
 
 if __name__ == "__main__":
